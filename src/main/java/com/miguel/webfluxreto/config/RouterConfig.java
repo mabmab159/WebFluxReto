87d505b5414048a455fdfbd4e6cf7a43a319e2eb
MabMab^^ <miguelberrioh@hotmail.com>
2023-10-13 01:14:30 -0500

    Agregando Handler Estudiante

--- src/main/java/com/miguel/webfluxreto/config/RouterConfig.java
+++ src/main/java/com/miguel/webfluxreto/config/RouterConfig.java
@@ -1,6 +1,7 @@
 package com.miguel.webfluxreto.config;
 
 import com.miguel.webfluxreto.handler.CursoHandler;
+import com.miguel.webfluxreto.handler.EstudianteHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.server.RouterFunction;
@@ -13,11 +14,20 @@ import static org.springframework.web.reactive.function.server.RouterFunctions.r
 public class RouterConfig {
 
     @Bean
-    public RouterFunction<ServerResponse> routes(CursoHandler cursoHandler) {
+    public RouterFunction<ServerResponse> routesCurso(CursoHandler cursoHandler) {
         return route(GET("/v2/curso"), cursoHandler::findAll)
                 .andRoute(GET("/v2/curso/{id}"), cursoHandler::findById)
                 .andRoute(POST("/v2/curso/{id}"), cursoHandler::create)
                 .andRoute(PUT("/v2/curso/{id}"), cursoHandler::updateById)
                 .andRoute(DELETE("/v2/curso/{id}"), cursoHandler::delete);
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> routesEstudiante(EstudianteHandler estudianteHandler){
+        return route(GET("/v2/estudiante"), estudianteHandler::findAll)
+                .andRoute(GET("/v2/estudiante/{id}"), estudianteHandler::findById)
+                .andRoute(POST("/v2/estudiante/{id}"), estudianteHandler::create)
+                .andRoute(PUT("/v2/estudiante/{id}"), estudianteHandler::updateById)
+                .andRoute(DELETE("/v2/estudiante/{id}"), estudianteHandler::delete);
     }
 }
--- src/main/java/com/miguel/webfluxreto/handler/EstudianteHandler.java
+++ src/main/java/com/miguel/webfluxreto/handler/EstudianteHandler.java
@@ -1,4 +1,75 @@
 package com.miguel.webfluxreto.handler;
 
+import com.miguel.webfluxreto.models.Estudiante;
+import com.miguel.webfluxreto.service.EstudianteService;
+import com.miguel.webfluxreto.validator.RequestValidator;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Mono;
+
+import java.net.URI;
+
+@Component
+@RequiredArgsConstructor
 public class EstudianteHandler {
+    private final EstudianteService estudianteService;
+    private final RequestValidator requestValidator;
+
+    public Mono<ServerResponse> findAll(ServerRequest req) {
+        return ServerResponse.ok()
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(estudianteService.findAll(), Estudiante.class);
+    }
+
+    public Mono<ServerResponse> findById(ServerRequest req) {
+        String id = req.pathVariable("id");
+        return ServerResponse.ok()
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(BodyInserters
+                        .fromValue(estudianteService.findById(id))
+                );
+    }
+
+    public Mono<ServerResponse> create(ServerRequest req) {
+        return req.bodyToMono(Estudiante.class)
+                .flatMap(this.requestValidator::validate)
+                .flatMap(e -> ServerResponse.created(
+                                        URI.create(req.uri()
+                                                .toString()
+                                                .concat("/")
+                                                .concat(e.getId()))
+                                )
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .body(BodyInserters.fromValue(e))
+                );
+    }
+
+    public Mono<ServerResponse> updateById(ServerRequest req) {
+        String id = req.pathVariable("id");
+        return req.bodyToMono(Estudiante.class)
+                .map(e -> {
+                    Mono<Estudiante> p = estudianteService.findById(id);
+                    if (p != null) {
+                        e.setId(id);
+                    }
+                    return e;
+                })
+                .flatMap(this.requestValidator::validate)
+                .flatMap(p -> ServerResponse.ok()
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .body(BodyInserters.fromValue(estudianteService.save(p))));
+    }
+
+    public Mono<ServerResponse> delete(ServerRequest req) {
+        String id = req.pathVariable("id");
+        return estudianteService.deleteById(id)
+                .flatMap(e ->
+                        e ? ServerResponse.noContent().build() :
+                                ServerResponse.notFound().build()
+                );
+    }
 }
